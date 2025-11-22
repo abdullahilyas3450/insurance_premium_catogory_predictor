0e57bc142e46beea93bdc0c76244ce86087d4969
Abdullah Ilyas <144126965+abdullahilyas3450@users.noreply.github.com>
2025-11-22 20:11:02 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from schema.UserInfo import UserInfo
-from model.predict import predict_insurance_premium, MODEL_VERSION
-
+from model.predict import predict_insurance_premimium, MODEL_VERSION
 
 
 app = FastAPI()
-
-
 
 @app.get("/")
 def home():
@@ -18,23 +15,29 @@ def health_check():
     return {"status": "ok",
             "version": MODEL_VERSION}
 
+
 @app.post("/predict")
 def predict(data: UserInfo):
     try:
-        #convert it in dict
+    # takes only the required parameters for the model and also convert it into dictionary
         input_df ={
-        'bmi': data.bmi,
-        'age_group': data.age_group,
-        'lifestyle_risk': data.lifestyle_risk,
-        'city_tier': data.city_tier,
-        'income_lpa': data.income_lpa,
-        'occupation': data.occupation
-    }
-        # Make prediction using the pre-loaded model
-        prediction = predict_insurance_premium(input_df)
-        
-        # Return the prediction as a JSON response
-        return JSONResponse(status_code=200,content={"predicted_insurance_premium": prediction[0]})
+            'bmi': data.bmi,
+            'age_group': data.age_group,
+            'lifestyle_risk': data.lifestyle_risk,
+            'city_tier': data.city_tier,
+            'income_lpa': data.income_lpa,
+            'occupation': data.occupation
+        }
+        # making prediction of the model and saving it in the prediction variable        
+        prediction = predict_insurance_premimium(input_df)
+
+        # sending the json response if success
+        return JSONResponse(status_code=200, content={'predicted_insurance_premium': prediction[0]})
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", reload=True)
